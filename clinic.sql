35872a5529990e2d1ca099e93b11a235aa14fbe3
Allen Buenaventura <115561893+allenbnvtra@users.noreply.github.com>
2024-12-19 13:15:31 +0800

    sql

--- clinic.sql
+++ clinic.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Dec 12, 2024 at 03:56 PM
--- Server version: 11.7.1-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2024 at 06:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,6 +37,13 @@ CREATE TABLE `audittrails` (
   `patientSupplyID` int(11) NOT NULL,
   `patientType` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `audittrails`
+--
+
+INSERT INTO `audittrails` (`patientID`, `patientUserID`, `patientEmail`, `patientIllnessType`, `patientDescription`, `patientDate`, `patientSupplyID`, `patientType`) VALUES
+(1, 0, 'allen@gmail.com', 1, 'You have cough and colds', '2024-12-13 00:00:00', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -92,7 +99,6 @@ CREATE TABLE `inventory` (
   `stock` int(11) NOT NULL,
   `startDate` date NOT NULL,
   `expDate` date NOT NULL,
-  `dispense` int(11) NOT NULL,
   `status` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -100,11 +106,31 @@ CREATE TABLE `inventory` (
 -- Dumping data for table `inventory`
 --
 
-INSERT INTO `inventory` (`supplyID`, `supplyName`, `stock`, `startDate`, `expDate`, `dispense`, `status`) VALUES
-(1, 'Biogesic', 4, '2024-12-01', '0000-00-00', 0, 'In Stock'),
-(2, 'Buscopan', 4, '2024-12-01', '0000-00-00', 0, 'In Stock'),
-(3, 'Diatabs', 0, '2024-12-01', '2028-12-01', 0, 'Out of Stock'),
-(4, 'Neozep', 10, '2024-12-01', '2028-12-01', 0, 'In Stock');
+INSERT INTO `inventory` (`supplyID`, `supplyName`, `stock`, `startDate`, `expDate`, `status`) VALUES
+(1, 'Biogesic', 4, '2024-12-01', '0000-00-00', 'In Stock'),
+(2, 'Buscopan', 4, '2024-12-01', '0000-00-00', 'In Stock'),
+(3, 'Diatabs', 0, '2024-12-01', '2028-12-01', 'Out of Stock'),
+(4, 'Neozep', 10, '2024-12-01', '2028-12-01', 'In Stock'),
+(5, 'Paracetamol', 500, '2023-12-01', '2025-06-01', 'In Stock'),
+(6, 'Ibuprofen', 300, '2023-11-15', '2025-05-15', 'In Stock'),
+(7, 'Amoxicillin', 250, '2023-12-10', '2024-12-10', 'In Stock'),
+(8, 'Cetrizine', 400, '2023-10-20', '2024-11-20', 'In Stock'),
+(9, 'Vitamin C', 1000, '2023-08-01', '2025-01-01', 'In Stock'),
+(10, 'Metformin', 350, '2023-09-05', '2025-02-05', 'In Stock'),
+(11, 'Loperamide', 200, '2023-11-01', '2024-11-01', 'In Stock'),
+(12, 'Losartan', 150, '2023-07-15', '2024-07-15', 'In Stock'),
+(13, 'Mefenamic Acid', 450, '2023-06-01', '2024-06-01', 'In Stock'),
+(14, 'Dextromethorphan', 320, '2023-05-01', '2024-05-01', 'In Stock'),
+(15, 'Hydrocortisone', 180, '2023-01-01', '2024-12-01', 'In Stock'),
+(16, 'Salbutamol', 220, '2023-02-01', '2025-01-15', 'In Stock'),
+(17, 'Clarithromycin', 130, '2023-03-01', '2025-03-01', 'In Stock'),
+(18, 'Cloxacillin', 410, '2023-04-01', '2024-04-15', 'In Stock'),
+(19, 'Ciprofloxacin', 260, '2023-05-20', '2024-06-20', 'In Stock'),
+(20, 'Albuterol', 300, '2023-06-15', '2024-12-31', 'In Stock'),
+(21, 'Cetirizine Hydrochloride', 240, '2023-07-01', '2024-07-30', 'In Stock'),
+(22, 'Omeprazole', 370, '2023-08-10', '2025-08-10', 'In Stock'),
+(23, 'Simvastatin', 290, '2023-09-01', '2024-09-15', 'In Stock'),
+(24, 'Aspirin', 600, '2023-10-05', '2025-10-05', 'In Stock');
 
 -- --------------------------------------------------------
 
@@ -128,19 +154,22 @@ CREATE TABLE `users` (
   `userBarangay` varchar(100) NOT NULL,
   `userTown` varchar(100) NOT NULL,
   `userCity` varchar(100) NOT NULL,
-  `userProvince` varchar(100) NOT NULL
+  `userProvince` varchar(100) NOT NULL,
+  `deleted` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `userStudentID`, `userFName`, `userMName`, `userLName`, `userType`, `userEmail`, `userPassword`, `userBirthday`, `userWorkPosition`, `userInstitute`, `userSubject`, `userBarangay`, `userTown`, `userCity`, `userProvince`) VALUES
-(7, NULL, 'dave', 'B', 'Tolentino', 2, 'vincentdavetolentino.basc@gmail.com', '$2y$10$Blv7mdqdMXaX1/OOrn/uyu5W8pG48RCNkpv/8NfUBJf.syNNm4z3G', '2024-10-01', NULL, 'IEAT', NULL, 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan'),
-(8, NULL, 'Althea Shamel', 'B', 'Pangilinan', 3, 'altheamiranda.basc@gmail.com', '$2y$10$jolwyRxDv5qZJYbXHEOQpe4sJDrn2FNcvL/S7ZGjKKwBcpNT34Ime', '2024-09-01', 'Librarian', '', NULL, 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan'),
-(9, '2022000595', 'Margelyn', 'B', 'Gonzales', 1, 'margelynvijandre.basc@gmail.com', '$2y$10$AWIqlEP4v7SzzOCMKmaT7ew.u5VOHUMeTQE84PuhIl4LPErAp48QS', '2024-09-01', NULL, 'engineering', 'information_technology', 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan'),
-(12, NULL, 'Allen', 'B', 'Buenaventura', 5, 'Allen@gmail.com', '$2y$10$rVUb.1CInTziThUup8tWkO4vVsLxg6axbvIns91gf9.Un1Dswcl5S', '2024-09-01', 'nurse', '', NULL, 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan'),
-(15, '2022000288', 'Lester Arjay', 'B', 'Merino', 1, 'lesterarjaymerino.basc@gmail.com', '$2y$10$/NYPPiFz/J6Qn2i88c72eOxsEJKO9xL.8/9nzsWLzll0HmAv4wLWm', '2024-07-01', NULL, 'engineering', 'information_technology', 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan');
+INSERT INTO `users` (`userID`, `userStudentID`, `userFName`, `userMName`, `userLName`, `userType`, `userEmail`, `userPassword`, `userBirthday`, `userWorkPosition`, `userInstitute`, `userSubject`, `userBarangay`, `userTown`, `userCity`, `userProvince`, `deleted`) VALUES
+(7, NULL, 'Dave Vincent', 'B', 'Tolentino', 2, 'vincentdavetolentino.basc@gmail.com', '$2y$10$Blv7mdqdMXaX1/OOrn/uyu5W8pG48RCNkpv/8NfUBJf.syNNm4z3G', '2024-10-01', NULL, 'IEAT', NULL, 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan', 1),
+(8, NULL, 'Althea Shamel', 'B', 'Pangilinan', 3, 'altheamiranda.basc@gmail.com', '$2y$10$jolwyRxDv5qZJYbXHEOQpe4sJDrn2FNcvL/S7ZGjKKwBcpNT34Ime', '2024-09-01', 'Librarian', 'IEAT', NULL, 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan', 0),
+(9, '2022000595', 'Margelyn', 'V.', 'Gonzales', 1, 'margelynvijandre.basc@gmail.com', '$2y$10$AWIqlEP4v7SzzOCMKmaT7ew.u5VOHUMeTQE84PuhIl4LPErAp48QS', '2024-09-01', NULL, 'engineering', 'information_technology', 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan', 0),
+(12, NULL, 'Allen', 'B', 'Buenaventura', 5, 'Allen@gmail.com', '$2y$10$rVUb.1CInTziThUup8tWkO4vVsLxg6axbvIns91gf9.Un1Dswcl5S', '2024-09-01', 'nurse', 'IEAT', NULL, 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan', 0),
+(15, '2022000288', 'Lester Arjay', 'B', 'Merino', 1, 'lesterarjaymerino.basc@gmail.com', '$2y$10$/NYPPiFz/J6Qn2i88c72eOxsEJKO9xL.8/9nzsWLzll0HmAv4wLWm', '2024-07-01', NULL, 'engineering', 'information_technology', 'Sampaguita St.', 'San Juan', 'San Ildefonso', 'Bulacan', 0),
+(16, '2023004931', 'Test', 'U', 'User', 1, 'testuser@gmail.com', '$2y$10$NqP0vSS6ACYMJYz7u7VTaumy/rfAEW3MjG7eQSJj1sF/doov.X7KG', '2024-12-12', NULL, 'agriculture', 'crop_science', 'Lapnit ', 'San Ildefonso', 'Bulacan', 'PJ', 0),
+(17, '124', 'Allen', 'V', 'Buenaventura', 1, 'allenbnvtra.11@gmail.com', '$2y$10$5xDJGSOcsMyN2uNSD138Lu0giegQf9U7zYNAfnouHvooNXTkccCNC', '2024-12-14', NULL, 'agriculture', 'crop_science', 'Lapnit', 'Lapnit', 'San ildefonso', 'Bulacan', 0);
 
 -- --------------------------------------------------------
 
@@ -244,7 +273,7 @@ ALTER TABLE `validation`
 -- AUTO_INCREMENT for table `audittrails`
 --
 ALTER TABLE `audittrails`
-  MODIFY `patientID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `patientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `audittype`
@@ -262,13 +291,13 @@ ALTER TABLE `illnesstypes`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `supplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `supplyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `usertype`
